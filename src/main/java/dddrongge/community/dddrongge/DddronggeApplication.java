dae5d8d31013da3dfb96ae36525b251adca9c091
sunzuorong <492793020@qq.com>
2019-08-25 16:59:19 +0800

    init repo

---
+++ src/main/java/dddrongge/community/dddrongge/DddronggeApplication.java
@@ -0,0 +1,13 @@
+package dddrongge.community.dddrongge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddronggeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddronggeApplication.class, args);
+    }
+
+}
